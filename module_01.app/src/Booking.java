558fe9253ccc8ac2d3c5e320fc9b640c2c9671eb
Arif Hajiyev <160994065+Arif-hckr1@users.noreply.github.com>
2025-04-29 08:23:18 -0700

    Create Booking.java

---
+++ module_01.app/src/Booking.java
@@ -0,0 +1,13 @@
+public class Booking {
+    public String bookingid;
+    public String name;
+    public String flightid;
+    public int numberoftickets;
+    public Booking(){}
+    public Booking(String bookingid, String name, String flightid, int numberoftickets){
+        this.bookingid = bookingid;
+        this.name = name;
+        this.flightid = flightid;
+        this.numberoftickets = numberoftickets;
+    }
+}
