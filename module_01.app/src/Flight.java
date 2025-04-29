406212dea148bb2f89c4798c6bcf9ac67531f16f
Arif Hajiyev <160994065+Arif-hckr1@users.noreply.github.com>
2025-04-29 08:22:36 -0700

    Create Flight.java

---
+++ module_01.app/src/Flight.java
@@ -0,0 +1,13 @@
+public class Flight {
+    public String flightid;
+    public String destination;
+    public String date;
+    public int freeSeats;
+    public Flight(){}
+    public Flight(String flightid,String destination, String date, int freeSeats){
+        this.flightid = flightid;
+        this.destination = destination;
+        this.date = date;
+        this.freeSeats = freeSeats;
+    }
+}
