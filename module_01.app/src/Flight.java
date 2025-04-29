1723e0ccf61f44a679c7cd9ebb7686a0eed144ac
Arif Hajiyev <160994065+Arif-hckr1@users.noreply.github.com>
2025-04-29 12:43:29 -0700

    Update Flight.java

--- module_01.app/src/Flight.java
+++ module_01.app/src/Flight.java
@@ -1,13 +1,27 @@
 public class Flight {
-    public String flightid;
-    public String destination;
-    public String date;
-    public int freeSeats;
-    public Flight(){}
+    private String flightid;
+    private String destination;
+    private String date;
+    private int freeSeats;
+    private Flight(){}
     public Flight(String flightid,String destination, String date, int freeSeats){
         this.flightid = flightid;
         this.destination = destination;
         this.date = date;
         this.freeSeats = freeSeats;
     }
+    public String getflightid(){
+        return flightid;
+    }
+    public String getdestination(){
+        return destination;
+    }
+    public String getdate(){
+        return date;
+    }
+    public String getfreeSeats(){
+        return freeSeats;
+    }
+    
+    
 }
