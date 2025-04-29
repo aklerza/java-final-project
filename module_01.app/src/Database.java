6e5cc609f1d77b409a93e4dd88171aa42cf5da1c
Arif Hajiyev <160994065+Arif-hckr1@users.noreply.github.com>
2025-04-29 08:33:46 -0700

    Update Database.java

--- module_01.app/src/Database.java
+++ module_01.app/src/Database.java
@@ -1,9 +1,74 @@
-public class Database {
-    public void load() {
+import java.util.*;
+import java.io.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+public class Database{
+    public List<Flight> flight = new ArrayList<>();
+    public List<Booking> booking = new ArrayList<>();
+    private File databaseFile = new File("database.txt");
+    private ObjectMapper newmapper = new ObjectMapper();
+    public Database(){
+        try{
+            if(databaseFile.exists()){
+                Database saving = newmapper.readValue(databaseFile, Database.class);
+                this.flight = saving.flight;
+                this.booking = saving.booking;
+            }
+        } catch (Exception e) {System.out.println("Error! -> " + e.getMessage());}
+    }
+    
+    public void savetofile(){
+        try{
+            newmapper.writerWithDefaultPrettyPrinter().writeValue(databaseFile, this);
+        } catch (Exception e){System.out.println("Error! -> " + e.getMessage());}
     }
 
-    public void save() {
+    public String JsonValuesFlight(){
+        try {
+            return newmapper.writerWithDefaultPrettyPrinter().writeValueAsString(this.flight);
+        } catch (Exception e) {
+            return "Smth went wrong , pffff!!";
+        }
+    }
 
+    public String JsonValuesBookings(){
+        try {
+            return newmapper.writerWithDefaultPrettyPrinter().writeValueAsString(this.booking);
+        } catch (Exception e) {
+            return "Smth went wrong , pffff!!";
+        }
+    }
+
+    public void BookFlight(String name, String flightid, int numberoftickets){
+        int t=0;
+        for(Flight f : this.flight){
+            if(f.flightid.equals(flightid) && f.freeSeats>=numberoftickets){
+                String bookingid="" + (this.booking.size() + 1);
+                this.booking.add(new Booking(bookingid,name,flightid,numberoftickets));
+                f.freeSeats-=numberoftickets;
+                this.savetofile();
+                System.out.println("Reserved successfully! ");
+                t=1;
+            }
+        }
+        if(t==0){System.out.println("Invalid Input! ");}
+    }
+
+    public void CancelBooking(String bookingid){
+        int t=0;
+        for(Booking b : this.booking){
+            if(b.bookingid.equals(bookingid)){
+                for(Flight f : this.flight){
+                    if(f.flightid.equals(b.flightid)){
+                        f.freeSeats+=b.numberoftickets;
+                    }
+                }
+                this.booking.remove(b);
+                this.savetofile();
+                System.out.println("Reservation cancelled successfully! ");
+                t=1;
+            }
+        }
+        if(t==0){System.out.println("Reservation not found! ");}
     }
 }
