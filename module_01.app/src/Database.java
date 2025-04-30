7688b574edc92acaa4549279c9d301a722e579d8
Arif Hajiyev <160994065+Arif-hckr1@users.noreply.github.com>
2025-04-29 18:49:45 -0700

    Update Database.java

--- module_01.app/src/Database.java
+++ module_01.app/src/Database.java
@@ -1,116 +1,186 @@
 import java.util.*;
 import java.io.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Database{
-    private List<Flight> flight = new ArrayList<>();
-    private List<Booking> booking = new ArrayList<>();
-    private File databaseFile = new File("database.json");
-    private ObjectMapper newmapper = new ObjectMapper();
-
-    public Database(){
-        load();
-    }
-
-    private void load(){
-        try{
-            if(databaseFile.exists()){
-                Database saving = newmapper.readValue(databaseFile, Database.class);
-                this.flight = saving.flight;
-                this.booking = saving.booking;
-            }
-        } catch (Exception e) {System.out.println("Error! -> " + e.getMessage());}
-    }
-
-    public void savetofile(){
-        try{
-            newmapper.writerWithDefaultPrettyPrinter().writeValue(databaseFile, this);
-        } catch (Exception e){System.out.println("Error! -> " + e.getMessage());}
-    }
-//from json data
-    public String AddFlight(String value) {
-        String flightid = null;
+	
+	@JsonProperty("flights")
+	public List<Flight> flights = new ArrayList<>();
+	
+	@JsonProperty("bookings")
+	public List<Booking> bookings = new ArrayList<>();
+	
+	private static final File DBFile = new File("database.json");
+	
+	public void saveDatabase() {
+		ObjectMapper mapper = new ObjectMapper();
         try {
-            Flight f = newmapper.readValue(value, Flight.class);
-            flightid = UUID.randomUUID().toString().replace("-", "").substring(0, 8);
-            f.setflightid(flightid);
-            this.flight.add(f);
-            this.savetofile();
-            return "{\"status\": 1,\"flightid\": \"" +flightid + "\"}";
-        }catch (Exception e) {
-
-            return "{\"status\": 0,\"flightid\": \"" +flightid + "\"}";;
+            mapper.writerWithDefaultPrettyPrinter().writeValue(DBFile, this);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
-    }
-
-    public String RemoveFlight(String value) {
-        try{
-           Map<String, String> data = newmapper.readValue(value,Map.class);
-           String flightid = data.get("flightid");
-           int t=0;
-           for (Flight f : this.flight) {
-               if (f.getflightid().equals(flightid)) {
-                   this.flight.remove(f);
-                   t=1;
-                   break;
-               }
-           }
-           this.savetofile();
-           if (t==0){return "{\"status\": 0}";}
-           else{return "{\"status\": 1}";}
-        }catch (Exception e) {
-            return "{\"status\": 0}";
-        }
-    }
-
-    public String AddBooking(String value) {
-        String bookingid = null;
+	}
+	
+	public void loadDatabase() {
+        ObjectMapper mapper = new ObjectMapper();
         try {
-            Booking b = newmapper.readValue(value, Booking.class);
-            bookingid = UUID.randomUUID().toString().replace("-", "").substring(0, 8);
-            b.setbookingid(bookingid);
-            int t=0;
-            for (Flight f : this.flight) {
-                if (f.getflightid().equals(b.getflightid())) {
-                    if (f.getfreeSeats()>=b.getnumberoftickets()){
-                        this.booking.add(b);
-                        f.setfreeSeats(f.getfreeSeats()-b.getnumberoftickets());
-                        t=1;
-                        break;
-                    }
-                }
+            if (DBFile.exists()) {
+            	Database db = mapper.readValue(DBFile, Database.class);
+                this.flights = db.flights;
+                this.bookings = db.bookings;
             }
-            this.savetofile();
-            if (t==0){return "{\"status\": 0,\"bookingid\": \"" +bookingid + "\"}";}
-            else{return "{\"status\": 1,\"bookingid\": \"" +bookingid + "\"}";}
-        }catch (Exception e) {
-
-            return "{\"status\": 0,\"bookingid\": \"" +bookingid + "\"}";
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
     }
+	
+	
+	/*
+	 	{
+			"destination": "DESTINATION",
+			"date": "FLIGHT_DATE",
+			"freeseats": 0
+		}
+	 */
+	public String AddFlight(String payload) {
+		try { 
+			
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode node = mapper.readTree(payload);
+			
+			String random_hex = Integer.toHexString(new Random().nextInt());
+			
+			Flight temp = new Flight(
+					random_hex,
+					node.get("destination").asText(),
+					node.get("date").asText(),
+					node.get("freeseats").asInt()
+					);
+			
+			this.flights.add(temp);
+			
+			this.saveDatabase();
+			
+			return "{ \"status\":1,\"flightid\":\"+random_hex+\" }";
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	
+	/*
+	 *  {
+			"flightid": "3e89a7ec"
+		}
+	*/
+	public String RemoveFlight(String payload) {
+		
+		try { 
+	        ObjectMapper mapper = new ObjectMapper();
+	        JsonNode node = mapper.readTree(payload);
+	        
+	        String flightid = node.get("flightid").asText();
+	        
+	        for (Flight flight : this.flights) {
+	            if (flight.flightid.equals(flightid)) {
+	                this.flights.remove(flight);
+	                break;
+	            }
+	        }
 
-    public String CancelBooking(String value) {
-        try{
-            Map<String, String> data = newmapper.readValue(value,Map.class);
-            String bookingid = data.get("bookingid");
-            int t=0;
-            for (Booking b : this.booking) {
-                if (b.getbookingid().equals(bookingid)) {
-                    for (Flight f : this.flight) {
-                        if (f.getflightid().equals(b.getflightid())) {
-                            f.setfreeSeats(f.getfreeSeats()+b.getnumberoftickets());
-                            this.booking.remove(b);
-                            t=1;
-                            break;
-                        }
-                    }
-                }
-            }
-            this.savetofile();
-            if (t==0){return "{\"status\": 0}";}
-            else{return "{\"status\": 1}";}
-        }catch (Exception e) {
-            return "{\"status\": 0}";
-        }
-    }
+	        this.saveDatabase();
+	        
+	        return "{ \"status\":1 }";
+	    } catch (JsonProcessingException e) {
+	    	throw new RuntimeException(e);
+	    }
+		
+	}
+	
+	
+	/*
+	 * {
+  			"flightid": "3e89a7ec",
+  			"name": "John Doe",
+  			"numberoftickets": 1
+		}*/
+	public String AddBooking(String payload) {
+		
+		try { 
+	        ObjectMapper mapper = new ObjectMapper();
+	        JsonNode node = mapper.readTree(payload);
+	        
+	        String flightId = node.get("flightid").asText();
+	        String name = node.get("name").asText();
+	        int numberOfTickets = node.get("numberoftickets").asInt();
+	        
+	        Flight flight = null;
+	        for (Flight f : this.flights) {
+	            if (f.flightid.equals(flightId)) {
+	                flight = f;
+	                break;
+	            }
+	        }
+	        
+	        if (flight != null && flight.freeSeats >= numberOfTickets) {
+	            String bookingId = Integer.toHexString(new Random().nextInt());
+	            Booking newBooking = new Booking(bookingId, name, flightId, numberOfTickets);
+	            this.bookings.add(newBooking);
+	            
+	            flight.freeSeats -= numberOfTickets;
+	            
+	            this.saveDatabase();
+	            
+	            return "{ \"status\":1, \"bookingid\":\"" + bookingId + "\" }";
+	        } else {
+	            return "{ \"status\":0, \"bookingid\":\"\" }";
+	        }
+	    } catch (JsonProcessingException e) {
+	    	throw new RuntimeException(e);
+	    }
+		
+	}
+	
+	
+	
+	/*
+	 * 
+	 * {
+  "bookingid": "a2c1b289"
+}*/
+	public String CancelBooking(String payload) {
+		
+		try { 
+	        ObjectMapper mapper = new ObjectMapper();
+	        JsonNode node = mapper.readTree(payload);
+	        
+	        String bookingid = node.get("bookingid").asText();
+	        
+	        for (Booking booking : this.bookings) {
+	            if (booking.bookingid.equals(bookingid)) {
+	                this.bookings.remove(booking);
+
+	                for (Flight flight : this.flights) {
+	                    if (flight.flightid.equals(booking.flightid)) {
+	                        flight.freeSeats += booking.numberoftickets;
+	                        break;
+	                    }
+	                }
+	                
+	                break;
+	            }
+	        }
+	        
+	        this.saveDatabase();
+	        
+	        return "{ \"status\":1 }";
+	    } catch (JsonProcessingException e) {
+	    	throw new RuntimeException(e);
+	    }
+		
+	}
 }
