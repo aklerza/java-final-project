dbb6e5bccede9954ae394c01bf0f073abda97b1c
aklerza <60571063+aklerza@users.noreply.github.com>
2025-04-30 08:47:33 +0400

    interface implement

--- module_01.app/src/UserInterface.java
+++ module_01.app/src/UserInterface.java
@@ -1,25 +1,7 @@
-public class UserInterface {
-    public void show_mainmenu() {
-
-    }
-
-    public void show_onlineboard() {
-
-    }
-
-    public void show_flight_info() {
-
-    }
-
-    public void show_book_flight() {
-
-    }
-
-    public void show_cancel_flight() {
-
-    }
-
-    public void show_my_flights() {
-
-    }
+public interface UserInterface {
+	public void show_mainmenu();
+	public void show_flight_info();
+	public void show_book_flight();
+	public void show_cancel_flight();
+	public void show_my_flights();
 }
