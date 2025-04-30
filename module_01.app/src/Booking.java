0187acd2a0062340257e1e1e2ec43c2571e50414
Arif Hajiyev <160994065+Arif-hckr1@users.noreply.github.com>
2025-04-30 12:26:33 -0700

    Update Booking.java

--- module_01.app/src/Booking.java
+++ module_01.app/src/Booking.java
@@ -10,5 +10,28 @@ public class Booking {
         this.flightid = flightid;
         this.numberoftickets = numberoftickets;
     }
-    
+    public String getbookingid(){
+        return bookingid;
+    }
+    public String getname(){
+        return name;
+    }
+    public String getflightid(){
+        return flightid;
+    }
+    public int getnumberoftickets(){
+        return numberoftickets;
+    }
+    public void setbookingid(String bookingid){
+        this.bookingid = bookingid;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public void setflightid(String flightid){
+        this.flightid = flightid;
+    }
+    public void setnumberoftickets(int numberoftickets){
+        this.numberoftickets = numberoftickets;
+    }
 }
